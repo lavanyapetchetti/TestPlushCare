097935536b8315eedd2be4283ea9d4048b2921cf
lavanyapetchetti <99505509+lavanyapetchetti@users.noreply.github.com>
2022-02-12 21:04:13 -0800

    Update appointment.js

--- pages/appointment.js
+++ pages/appointment.js
@@ -20,7 +20,9 @@ const elements = {
  * home Functions
  */
 const pageCommands = {
-
+    /*
+     * Redirected to appointment url
+     */
     visit: function () {
         this
             .navigate(APPOINTMENT_URL)
@@ -29,7 +31,13 @@ const pageCommands = {
         return this;
     },
 
+    /*
+     * Selects second day from current day from date picker
+     */
     selectDateFromDatePicker: function (day) {
+        if (day === 0 || day === 30) {
+            this.assert.ok(false, 'Given day is not valid');
+        }
         const appointmentDate = moment(new Date()).add(day, 'days');
         const datePickerSelector = `#appointments-wrapper .react-datepicker__day[aria-label="Choose ${appointmentDate.format('dddd, MMMM Do, YYYY')}"]`;
         const inputDatePickerSelecotr = `#date-picker-input[value="${appointmentDate.format('MM/DD/YYYY')}"]`;
@@ -46,9 +54,12 @@ const pageCommands = {
         return this;
     },
 
+    /*
+     * Book an appointment with doctor whose rating is greater than 4.8
+     */
     selectDoctorFromDoctorList: function (rating) {
         if (rating < 4.8) {
-            this.assert.ok(false, 'Given rating is lot valid');
+            this.assert.ok(false, 'Given rating is not valid');
         } else {
             const selector = `//*[@data-testid="rating"]/following-sibling::span[text() >= "${rating}"]`;
             let self = this;
@@ -70,6 +81,52 @@ const pageCommands = {
                 }
             });
         }
+    },
+
+    /*
+     * Book an appointment with doctor by any rating
+     */
+    selectDoctorByRating: function (rating) {
+        if (rating < 0) {
+            this.assert.ok(false, 'Given rating is not valid');
+        } else {
+            const selector = `//*[@data-testid="rating"]/following-sibling::span[text() = "${rating}"]`;
+            let self = this;
+            this.api.elements('xpath', selector, function (elementCount) {
+                const isPresent = elementCount.value.length > 0;
+                if (isPresent) {
+                    let randomDoctor = Math.floor(Math.random() * elementCount.value.length);
+                    if (randomDoctor === 0){
+                        randomDoctor = randomDoctor + 1;
+                    }
+                    const bookButtonSelector = `(//*[@data-testid="rating"]/following-sibling::span[text() = "${rating}"])[${randomDoctor}]/../../following::div[2]//button`;
+                    self
+                        .api.useXpath()
+                        .waitForElementVisible(bookButtonSelector)
+                        .click(bookButtonSelector)
+                        .useCss();
+                } else {
+                    self.api.assert.ok(false, 'Unable to find doctor with given rating');
+                }
+            });
+        }
+    },
+
+
+    /*
+     * Book an appointment by nth doctor from the doctor's list
+     */
+    selectNthDoctorFromDoctorList: function (num) {
+        const selector = `(//div[contains(@class, "appointment ")]//button[text()="Book"])[${num + 1}]`;
+
+        this
+            .useXpath()
+            .waitForElementVisible(selector)
+            .click(selector)
+            .useCss();
+
+        return this;
+
     }
 
 };
