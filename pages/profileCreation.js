1d1b709e84ec386892d6ac4739407496d9d3f4e1
lavanyapetchetti <99505509+lavanyapetchetti@users.noreply.github.com>
2022-02-12 22:10:27 -0800

    Update profileCreation.js

--- pages/profileCreation.js
+++ pages/profileCreation.js
@@ -10,22 +10,76 @@ const elements = {
     rating: {
         selector: '//*[@data-testid="rating"]/following-sibling::span',
         locateStrategy: 'xpath'
+    },
+
+    firstName: 'input[name="first-name"]',
+
+    lastName: 'input[name="last-name"]',
+
+    dateOfBirth: 'input[name="dob"]',
+
+    phoneNumber: 'input[name="phone"]',
+
+    emailAddress: 'input[name="email"]',
+
+    password: 'input[name="password"]',
+
+    optIn: 'input[name="tos-opt-in"]',
+
+    continueButton: {
+        selector: '//button[text() = "Continue"]',
+        locateStrategy: 'xpath'
     }
 
 };
 
 /**
- * home Functions
+ * Profile Creation Page Functions
  */
 const pageCommands = {
-
-    VerifyProfileCreation: function () {
+    /*
+     * Verify Profile creation Page
+     */
+    VerifyRatingOnProfileCreation: function () {
         this
             .waitForElementVisible('@createProfile')
             .waitForElementVisible('@rating')
             .getText('@rating', function (result) {
                 this.assert.ok(result.value >= 4.8);
             });
+
+        return this;
+    },
+
+    /*
+     * Create profile
+     */
+    enterUserDetails: function (firstName, lastName, dob, gender, num, email, psw) {
+        const selector = `//input[@aria-label="${gender}"]`;
+
+        this
+            .waitForElementVisible('@firstName')
+            .setValue('@firstName', firstName)
+            .waitForElementVisible('@lastName')
+            .setValue('@lastName', lastName)
+            .waitForElementVisible('@dateOfBirth')
+            .setValue('@dateOfBirth', dob);
+        this
+            .useXpath()
+            .waitForElementVisible(selector)
+            .click(selector)
+            .useCss();
+        this
+            .waitForElementVisible('@phoneNumber')
+            .setValue('@phoneNumber', num)
+            .waitForElementVisible('@emailAddress')
+            .setValue('@emailAddress', email)
+            .waitForElementVisible('@password')
+            .setValue('@password', psw)
+            .waitForElementVisible('@optIn')
+            .click('@optIn')
+            .waitForElementVisible('@continueButton')
+            .click('@continueButton');
 
         return this;
     }
